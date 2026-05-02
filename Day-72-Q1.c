9753ca34075b95e17dcb40934b857cfa5b3b4f36
Ashna Dutta <Ashna.27808@stu.upes.ac.in>
2026-05-02 23:54:52 +0530

    Day 72: Added Q1

---
+++ Day-72-Q1.c
@@ -0,0 +1,42 @@
+//Given a string s consisting of lowercase letters, find the first repeated character in the string. A character is considered repeated if it appears more than once, and among all such characters, the one whose second occurrence has the smallest index should be returned.
+/*
+Input Format
+A single string s.
+
+Output Format
+Print the first repeated character. If no character is repeated, print -1.
+
+Sample Input
+geeksforgeeks
+
+Sample Output
+e
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    int count[26] = {0};  // for 'a' to 'z'
+
+    // Input string
+    scanf("%s", s);
+
+    // Traverse string
+    for (int i = 0; i < strlen(s); i++) {
+        int index = s[i] - 'a';  // map character to 0–25
+
+        count[index]++;
+
+        // If count becomes 2, it's the first repeated
+        if (count[index] == 2) {
+            printf("%c\n", s[i]);
+            return 0;
+        }
+    }
+
+    // If no repeated character found
+    printf("-1\n");
+
+    return 0;
+}
